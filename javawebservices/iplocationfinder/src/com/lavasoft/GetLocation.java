238eae38ddf7ac7894dc4430cde515c660927ce0
arocks-ai <28286480+arocks-ai@users.noreply.github.com>
2020-08-16 15:40:48 -0600

    Java SOAP Client

---
+++ javawebservices/iplocationfinder/src/com/lavasoft/GetLocation.java
@@ -0,0 +1,32 @@
+
+package com.lavasoft;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * &lt;p&gt;Java class for anonymous complex type.
+ * 
+ * &lt;p&gt;The following schema fragment specifies the expected content contained within this class.
+ * 
+ * &lt;pre&gt;
+ * &amp;lt;complexType&amp;gt;
+ *   &amp;lt;complexContent&amp;gt;
+ *     &amp;lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&amp;gt;
+ *     &amp;lt;/restriction&amp;gt;
+ *   &amp;lt;/complexContent&amp;gt;
+ * &amp;lt;/complexType&amp;gt;
+ * &lt;/pre&gt;
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "GetLocation")
+public class GetLocation {
+
+
+}
---
+++ javawebservices/iplocationfinder/src/com/lavasoft/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * &lt;b&gt;A web service which performs GetIpAddress Lookups.&lt;/b&gt;
+ * 
+ */
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://lavasoft.com/", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.lavasoft;
